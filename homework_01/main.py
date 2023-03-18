3779385532e0fafa5e41fd13037413aecb573fe0
Vitaliy Zlobin <zlovian@yandex.ru>
2023-03-18 16:58:03 +0700

    Add homework_01

---
+++ homework_01/main.py
@@ -0,0 +1,63 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*numbers):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+    return [number ** 2 for number in numbers]
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def get_odd_numbers(numbers_list):
+    return [number for number in numbers_list if number % 2 != 0]
+
+
+def get_even_numbers(numbers_list):
+    return [number for number in numbers_list if number % 2 == 0]
+
+
+def is_prime_number(number):
+    for n in range(2, number // 2 + 1):
+        if (number % n) == 0:
+            return False
+    return True
+
+
+def get_prime_numbers(numbers):
+    return [number for number in numbers if is_prime_number(number)]
+
+
+def filter_numbers(numbers, mode):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    match mode:
+        case "odd":
+            return get_odd_numbers(numbers)
+        case "even":
+            return get_even_numbers(numbers)
+        case "prime":
+            return get_prime_numbers(numbers)
+        # case _:
+        #     raise AttributeError
+
+
