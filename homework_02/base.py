da02a54bd94d176bc7b2ee98039d4a445d6533c1
Vitaliy Zlobin <zlovian@yandex.ru>
2023-04-09 13:59:31 +0700

    Add error message and "if  not self.started" in start method

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,25 +1,26 @@
 from abc import ABC
-import exceptions
+from exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
 
-    def __init__(self, weight: int = 0, fuel: int = 0, fuel_consumption: int = 0):
-        self.started = False
+    def __init__(self, weight: int, fuel: int, fuel_consumption: int):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
+        self.started = False
 
     def start(self):
         if self.fuel <= 0:
-            raise exceptions.LowFuelError
+            raise LowFuelError(f"{self.fuel=}")
 
-        self.started = True
+        if not self.started:
+            self.started = True
 
     def move(self, distance: int):
         max_distance = self.fuel // self.fuel_consumption if self.fuel_consumption > 0 else 0
 
         if max_distance < distance:
-            raise exceptions.NotEnoughFuel
+            raise NotEnoughFuel(f"{self.fuel=}, {self.fuel_consumption=} | {distance=} < {max_distance=}")
 
         self.fuel -= distance * self.fuel_consumption
